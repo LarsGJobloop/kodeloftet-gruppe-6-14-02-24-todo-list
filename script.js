317cade7730b8b9c288d333a3b75f6c110f2653e
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-14 11:31:07 +0100

    add steps for creating a task element

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello!');
+const task1 = {
+    title: "Sunbath",
+}
+
+// createElement for å lage nye elemneter
+const liWrapper = document.createElement("li")
+const containerElement = document.createElement("div")
+const taskTitle = document.createElement("h2")
+
+// className kan brukes for å sette klassenavn
+containerElement.className = "task"
+
+// textConten kan brukes for å sette teks innhold
+taskTitle.textContent = task1.title
+
+// appendChild brukes for å kombiner elementer
+containerElement.appendChild(taskTitle)
+liWrapper.appendChild(containerElement)
+
+// Finne ut hvor i HTML (DOMet) vi ønsker å sette inn
+// det nye elementet vårt
+// Kan bruke .qeuerySelector for å finne elmenter
+// ved samme syntakst som CSS selector
+const taskList = document.querySelector(".task-list")
+
+// Legg til det nye elementet vårt
+taskList.appendChild(liWrapper)
