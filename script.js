93bd089e5f70ec8dc10a95b757bfc0060e70db67
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-14 12:05:39 +0100

    button now creates new tasks when clicked

--- script.js
+++ script.js
@@ -1,28 +1,57 @@
-// Objekt som inneholder informasjon om et gjøremål
-const task1 = {
-    title: "Sunbath",
+/**
+ * Funksjon som tar imot et task objekt
+ * genererer et nytt element og legger det til
+ * i HTML
+ */
+function createTask(task) {
+    // docuement.createElement for å lage nye elemneter
+    const liWrapper = document.createElement("li")
+    const containerElement = document.createElement("div")
+    const taskTitle = document.createElement("h2")
+
+    // element.className kan brukes for å sette klassenavn
+    containerElement.className = "task"
+
+    // element.textContent kan brukes for å sette tekst innhold
+    taskTitle.textContent = task.title
+
+    // element.appendChild brukes for å kombiner elementer
+    containerElement.appendChild(taskTitle)
+    liWrapper.appendChild(containerElement)
+
+    // Finne ut hvor i HTML (DOMet) vi ønsker å sette inn
+    // det nye elementet vårt
+    // Kan bruke document.qeuerySelector for å finne elementer
+    // ved samme syntakst vi bruker i CSS
+    const taskList = document.querySelector(".task-list")
+
+    // Legg til det nye elementet vårt
+    taskList.appendChild(liWrapper)
 }
 
-// docuement.createElement for å lage nye elemneter
-const liWrapper = document.createElement("li")
-const containerElement = document.createElement("div")
-const taskTitle = document.createElement("h2")
+// Finne hvordan kjøre koden når knappen blir trykt på
+// 1. Finn knappen
+const addTaskButton = document.querySelector("#add-task")
 
-// element.className kan brukes for å sette klassenavn
-containerElement.className = "task"
+// 2. Lage funksjon som skal kjøre når kappen trykkes
+/**
+ * Funksjon som skal kjøre når knappen blir trykket
+ * og skal lese av verdien i input feltet, lage et
+ * nytt Task objekt for å sende videre til createTask funksjonen vår
+ */
+function addTask() {
+    // Objekt som inneholder informasjon om et gjøremål
+    const task1 = {
+        title: "Sunbath",
+    }
 
-// element.textContent kan brukes for å sette tekst innhold
-taskTitle.textContent = task1.title
+    // Lage Task objektet vårt fra det som står skrevet i
+    // input feltet
+    
+    
+    // Gi til createTask funksjonen vår
+    createTask(task1)
+}
 
-// element.appendChild brukes for å kombiner elementer
-containerElement.appendChild(taskTitle)
-liWrapper.appendChild(containerElement)
-
-// Finne ut hvor i HTML (DOMet) vi ønsker å sette inn
-// det nye elementet vårt
-// Kan bruke document.qeuerySelector for å finne elementer
-// ved samme syntakst vi bruker i CSS
-const taskList = document.querySelector(".task-list")
-
-// Legg til det nye elementet vårt
-taskList.appendChild(liWrapper)
+// 3. Kjør funksjonen hver gang knappene trykkes
+addTaskButton.addEventListener("click", addTask)
