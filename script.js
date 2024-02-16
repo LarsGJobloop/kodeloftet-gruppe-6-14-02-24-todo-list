c1e153df974d5ff996dc2f771eb0d768c00874ff
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-16 12:04:57 +0100

    extract create task html into own function

--- script.js
+++ script.js
@@ -6,27 +6,17 @@ function addTask() {
     if (taskTitle === "") {
         console.warn("Du må skrive inn noe i tekst feltet")
     } else {
-        // 1. Konstruer Gjøremåls objektet objektet
+        // 1. Konstruer Gjøremåls objektet
         const newTask = {
             title: taskTitle,
         }
 
         // 2. Lag alle HTML elementene
-        const liElement = document.createElement("li")
-        const divElement = document.createElement("div")
-        const h2Element = document.createElement("h2")
-
-        // Konfigurer elementen
-        divElement.className = "task"
-        h2Element.textContent = newTask.title
-
-        // Koble de sammen
-        liElement.appendChild(divElement)
-        divElement.appendChild(h2Element)
+        const newTaskElement = createTaskElement(newTask)
 
         // 3. Legg til det nye elementet i HTML
         const taskList = document.querySelector("#task-list")
-        taskList.appendChild(liElement)
+        taskList.appendChild(newTaskElement)
 
         // 4. Tilbakestill tekst input feltet
         inputElement.value = ""
@@ -35,3 +25,20 @@ function addTask() {
 
 const addTaskButton = document.querySelector("#add-task")
 addTaskButton.addEventListener("click", addTask)
+
+function createTaskElement(newTask) {
+    // Lage alle HTML elementene
+    const liElement = document.createElement("li")
+    const divElement = document.createElement("div")
+    const h2Element = document.createElement("h2")
+
+    // Konfigurer elementen
+    divElement.className = "task"
+    h2Element.textContent = newTask.title
+
+    // Koble de sammen
+    liElement.appendChild(divElement)
+    divElement.appendChild(h2Element)
+
+    return liElement
+}
